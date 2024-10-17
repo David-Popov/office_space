76133de6cd1f9eefc7f7744a366fdc471e371c44
didicko2003 <didicko2003@gmail.com>
2024-10-17 17:33:46 +0300

    project init

---
+++ src/test/java/javawizzards/officespace/OfficespaceApplicationTests.java
@@ -0,0 +1,13 @@
+package javawizzards.officespace;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OfficespaceApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
